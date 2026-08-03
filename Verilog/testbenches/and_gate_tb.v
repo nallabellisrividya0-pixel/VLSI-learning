10b41c49745aa0f2f1a11e1d008187510daec88c
nallabellisrividya0-pixel <nallabellisrividya0@gmail.com>
2026-08-03 21:26:17 +0530

    Fix syntax errors in and_gate_tb.v

--- Verilog/testbenches/and_gate_tb.v
+++ Verilog/testbenches/and_gate_tb.v
@@ -3,7 +3,7 @@ module and_gate_tb;
   wire Y;
   and _gate uut(.A(A),B(B),Y((Y));
                 initial begin
-                  A+0; B=0; #10;
+                  A=0; B=0; #10;
                   A=0; B=1; #10;
                   A=1; B=0; #10;
                   A=1; B=1; #10;
