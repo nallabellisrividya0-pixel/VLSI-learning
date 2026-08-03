ee0bd6b4a1e86bee4561569e654721eca4ba368f
nallabellisrividya0-pixel <nallabellisrividya0@gmail.com>
2026-08-03 21:26:39 +0530

    Update or_gate_tb.v

--- Verilog/testbenches/or_gate_tb.v
+++ Verilog/testbenches/or_gate_tb.v
@@ -3,7 +3,7 @@ module or_gate_tb;
   wire Y;
   or_gate uut(.A(A),B(B),Y((Y));
                 initial begin
-                  A+0; B=0; #10;
+                  A=0; B=0; #10;
                   A=0; B=1; #10;
                   A=1; B=0; #10;
                   A=1; B=1; #10;
