0b67dd07f7db6baf9dc2248f1527c3f13c299a9a
nallabellisrividya0-pixel <nallabellisrividya0@gmail.com>
2026-08-03 22:11:03 +0530

    Fix port connection syntax in nand_gate instantiation

--- Verilog/testbenches/nand_gate_tb.v
+++ Verilog/testbenches/nand_gate_tb.v
@@ -1,7 +1,7 @@
 module nand_gate_tb;
   reg A,B;
   wire Y;
-  nand_gate uut(.A(A),B(B),Y(Y));
+  nand_gate uut(.A(A),.B(B),.Y(Y));
                 initial begin
                   A=0; B=0; #10;
                   A=0; B=1; #10;
