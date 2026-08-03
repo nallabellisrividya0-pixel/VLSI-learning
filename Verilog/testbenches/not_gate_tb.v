eefea3c9ea5e4f75ef93bb7ec02cc302d50e7a5b
nallabellisrividya0-pixel <nallabellisrividya0@gmail.com>
2026-08-03 22:13:50 +0530

    Format instantiation of not_gate for readability

--- Verilog/testbenches/not_gate_tb.v
+++ Verilog/testbenches/not_gate_tb.v
@@ -1,7 +1,9 @@
 module not_gate_tb;
   reg A;
   wire Y;
-  not_gate uut(.A(A),.Y(Y));
+  not_gate uut(.A(A),
+               .Y(Y)
+              );
                 initial begin
                   A=0; #10;
                   A=1; #10;
