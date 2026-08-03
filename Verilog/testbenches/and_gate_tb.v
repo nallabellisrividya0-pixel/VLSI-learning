10d2b6225fb3acf5fcf25a99560746e7bf234c78
nallabellisrividya0-pixel <nallabellisrividya0@gmail.com>
2026-08-03 21:23:17 +0530

    Add testbench for AND gate functionality

---
+++ Verilog/testbenches/and_gate_tb.v
@@ -0,0 +1,12 @@
+module and_gate_tb;
+  reg A,B;
+  wire Y;
+  and _gate uut(.A(A),B(B),Y((Y));
+                initial begin
+                  A+0; B=0; #10;
+                  A=0; B=1; #10;
+                  A=1; B=0; #10;
+                  A=1; B=1; #10;
+                  $finish;
+                end
+                endmodule
