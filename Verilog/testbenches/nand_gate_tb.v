ea5a38671de766d91dffbc760af482a9d5003f7d
nallabellisrividya0-pixel <nallabellisrividya0@gmail.com>
2026-08-03 21:29:09 +0530

    Add testbench for NAND gate functionality

---
+++ Verilog/testbenches/nand_gate_tb.v
@@ -0,0 +1,12 @@
+module nand_gate_tb;
+  reg A,B;
+  wire Y;
+  nand_gate uut(.A(A),B(B),Y((Y));
+                initial begin
+                  A=0; B=0; #10;
+                  A=0; B=1; #10;
+                  A=1; B=0; #10;
+                  A=1; B=1; #10;
+                  $finish;
+                end
+                endmodule
