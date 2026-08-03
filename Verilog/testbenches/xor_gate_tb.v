cc1b361707f7c44330b5beada8cd8098a19ebaf7
nallabellisrividya0-pixel <nallabellisrividya0@gmail.com>
2026-08-03 21:33:16 +0530

    Add testbench for XOR gate

---
+++ Verilog/testbenches/xor_gate_tb.v
@@ -0,0 +1,12 @@
+module xor_gate_tb;
+  reg A,B;
+  wire Y;
+  xor_gate uut(.A(A),B(B),Y((Y));
+                initial begin
+                  A=0; B=0; #10;
+                  A=0; B=1; #10;
+                  A=1; B=0; #10;
+                  A=1; B=1; #10;
+                  $finish;
+                end
+                endmodule
