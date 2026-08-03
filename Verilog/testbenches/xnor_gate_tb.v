22423089f98adbc2a3a5738573bf428cac23139e
nallabellisrividya0-pixel <nallabellisrividya0@gmail.com>
2026-08-03 21:35:05 +0530

    Fix syntax errors in xnor_gate_tb module

--- Verilog/testbenches/xnor_gate_tb.v
+++ Verilog/testbenches/xnor_gate_tb.v
@@ -1,1 +1,13 @@
 
+module xnor_gate_tb;
+  reg A,B;
+  wire Y;
+  xnor_gate uut(.A(A),B(B),Y((Y));
+                initial begin
+                  A=0; B=0; #10;
+                  A=0; B=1; #10;
+                  A=1; B=0; #10;
+                  A=1; B=1; #10;
+                  $finish;
+                end
+                endmodule
