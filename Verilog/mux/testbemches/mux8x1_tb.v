8a1f110cc39a4552bd037181d1f90db6d27d371d
nallabellisrividya0-pixel <nallabellisrividya0@gmail.com>
2026-08-04 22:46:05 +0530

    Add testbench for 8x1 multiplexer

---
+++ Verilog/mux/testbemches/mux8x1_tb.v
@@ -0,0 +1,28 @@
+module tb_mux8x1;
+
+reg [7:0] in;
+reg [2:0] sel;
+wire y;
+
+mux8x1 uut(
+    .in(in),
+    .sel(sel),
+    .y(y)
+);
+
+integer i;
+
+initial
+begin
+    in=8'b10110010;
+
+    for(i=0;i<8;i=i+1)
+    begin
+        sel=i;
+        #10;
+    end
+
+    $finish;
+end
+
+endmodule
