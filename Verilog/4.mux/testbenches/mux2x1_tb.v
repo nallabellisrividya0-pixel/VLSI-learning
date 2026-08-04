8058a8389082da530f0278fd82a52a931dc9e62c
nallabellisrividya0-pixel <nallabellisrividya0@gmail.com>
2026-08-04 23:21:11 +0530

    Update and rename mux2x1_tb.v to mux2x1_tb.v

--- Verilog/4.mux/testbenches/mux2x1_tb.v
+++ Verilog/4.mux/testbenches/mux2x1_tb.v
@@ -1,19 +1,13 @@
-
-
 module tb_mux2x1;
-
 reg a, b, sel;
 wire y;
-
 mux2x1 uut (
     .a(a),
     .b(b),
     .sel(sel),
     .y(y)
 );
-
 integer i;
-
 initial
 begin
     $monitor("Time=%0t a=%b b=%b sel=%b y=%b", $time,a,b,sel,y);
