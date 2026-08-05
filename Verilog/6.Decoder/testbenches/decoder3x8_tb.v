96ddbbabfb830ecb869ea7c0ad1c7b97f3087510
nallabellisrividya0-pixel <nallabellisrividya0@gmail.com>
2026-08-05 16:04:49 +0530

    Add testbench for 3-to-8 decoder

--- Verilog/6.Decoder/testbenches/decoder3x8_tb.v
+++ Verilog/6.Decoder/testbenches/decoder3x8_tb.v
@@ -1,1 +1,23 @@
-
+module decoder3x8_tb;
+reg [2:0] A;
+reg EN;
+wire [7:0] Y;
+decoder3x8 uut(
+    .A(A),
+    .EN(EN),
+    .Y(Y)
+);
+integer i;
+initial
+begin
+    EN=1;
+  for(i=0;i<8;i=i+1)
+    begin
+        A=i;
+        #10;
+    end
+      EN=0;
+    #10;
+  $finish;
+end
+endmodule
